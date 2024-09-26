174ae6ec3558c2353cbcb16d15fece078617c4b1
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-26 20:43:10 +0200

    [UPD] deleted welcome.blade.php; added home.blade.php; updated README.md

---
+++ resources/views/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-migration-seeder</title>
+</head>
+<body>
+    <h1 class="m-auto">Treni in partenza oggi</h1>
+    <hr>
+    <table>
+        <thead>
+            <tr>
+                <th>Azienda</th>
+                <th>Stazione di partenza</th>
+                <th>Stazione di arrivo</th>
+                <th>Orario di partenza</th>
+                <th>Orario di arrivo</th>
+                <th>Codice treno</th>
+                <th>Numero carrozze</th>
+                <th>In orario</th>
+                <th>Cancellato</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($trains as $train)
+                <tr>
+                    <td>{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_partenza }}</td>
+                    <td>{{ $train->stazione_arrivo }}</td>
+                    <td>{{ $train->orario_partenza }}</td>
+                    <td>{{ $train->orario_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->numero_carrozze }}</td>
+                    <td>{{ $train->in_orario ? 'Sì' : 'No' }}</td>
+                    <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="9">Nessun treno in partenza oggi.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+</body>
+</html>
